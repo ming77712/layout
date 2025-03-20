c18a88598bdac4bc7fb6aa5f5f7e145a74a0b579
unknown <stevenhuang@pstcom.com.tw>
2025-03-20 10:49:34 +0800

    修改首頁內容

--- js/all.js
+++ js/all.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
+  const backdropBlur = document.querySelector('.backdrop-blur');
+  const infoWrapItem = document.querySelector('.infoWrapItem');
+  const headerInfoWrap = document.querySelector('.headerInfoWrap');
+
+  infoWrapItem.addEventListener('mouseenter', function () {
+    headerInfoWrap.classList.add('active');
+    backdropBlur.style.display = 'block';
+  });
+
+  headerInfoWrap.addEventListener('mouseenter', function () {
+    headerInfoWrap.classList.add('active');
+  });
+
+  headerInfoWrap.addEventListener('mouseleave', function (event) {
+    // 如果滑鼠進入 .infoWrapItem，則不移除 active 類別
+    if (!infoWrapItem.contains(event.relatedTarget)) {
+      headerInfoWrap.classList.remove('active');
+      backdropBlur.style.display = 'none';
+    }
+  });
+
   const newsSwiper = new Swiper('.newsSwiper', {
     loop: true,
 
