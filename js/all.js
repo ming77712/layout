40dca6dde01d936aa1839bcd40f0a63d288c63f7
unknown <stevenhuang@pstcom.com.tw>
2025-03-21 12:04:20 +0800

    修改登入樣式

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const header = document.querySelector('.header');
+  // header block start
   const backdropBlur = document.querySelector('.backdrop-blur');
   const infoWrapItem = document.querySelector('.infoWrapItem');
   const headerInfoWrap = document.querySelector('.headerInfoWrap');
@@ -14,12 +14,49 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   headerInfoWrap.addEventListener('mouseleave', function (event) {
-    // 如果滑鼠進入 .infoWrapItem，則不移除 active 類別
+    // 如果滑鼠進入 .infoWrapItem，則不移除 active
     if (!infoWrapItem.contains(event.relatedTarget)) {
       headerInfoWrap.classList.remove('active');
       backdropBlur.style.display = 'none';
     }
   });
+  // header block end
+
+  // login block start
+  const loginList = document.getElementById('loginList');
+  const userBtn = document.getElementById('userBtn');
+  const userWrap = document.getElementById('userWrap');
+  const userCloseBtn = document.getElementById('userCloseBtn');
+
+  userBtn.addEventListener('click', () => {
+    loginList.classList.add('d-none');
+    userWrap.classList.remove('d-none');
+  });
+
+  userCloseBtn.addEventListener('click', () => {
+    userWrap.classList.add('d-none');
+    loginList.classList.remove('d-none');
+  });
+
+  const selectLogin = document.getElementById('selectLogin');
+  const buttons = selectLogin.querySelectorAll('button');
+  const cardForm = document.getElementById('cardForm');
+  const accountForm = document.getElementById('accountForm');
+
+  selectLogin.addEventListener('click', (e) => {
+    buttons.forEach((button) => {
+      button.classList.remove('active');
+    });
+    e.target.classList.add('active');
+    if (e.target.dataset.selectLogin === 'card') {
+      accountForm.classList.add('d-none');
+      cardForm.classList.remove('d-none');
+    } else {
+      cardForm.classList.add('d-none');
+      accountForm.classList.remove('d-none');
+    }
+  });
+  // login block end
 
   const newsSwiper = new Swiper('.newsSwiper', {
     loop: true,
