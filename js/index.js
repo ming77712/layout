d8cfcd0a125636ebc3e438638f5ee3083384a731
unknown <stevenhuang@pstcom.com.tw>
2025-04-02 16:25:30 +0800

    修改圖形化數字呈現

--- js/index.js
+++ js/index.js
@@ -64,6 +64,71 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // login block end
 
+  const svgContainer = document.querySelector('.svg-container');
+
+  svgContainer.addEventListener('mouseover', (e) => {
+    const region = e.target.closest('.region');
+    if (region) {
+      const regionName = region.getAttribute('data-region');
+      if (regionName && regionValue[regionName]) {
+        const regionLabel = region.querySelector('.regionLabel');
+
+        regionLabel.classList.add('showing-value');
+
+        setTimeout(() => {
+          regionLabel.textContent = regionValue[regionName];
+          regionLabel.classList.remove('showing-value');
+          regionLabel.classList.add('value-shown');
+        }, 150);
+      }
+    }
+  });
+
+  svgContainer.addEventListener('mouseout', (e) => {
+    const region = e.target.closest('.region');
+    const toRegion = e.relatedTarget?.closest('.region');
+
+    if (region && region !== toRegion) {
+      const regionName = region.getAttribute('data-region');
+      if (regionName) {
+        const regionLabel = region.querySelector('.regionLabel');
+
+        regionLabel.classList.add('showing-value');
+        regionLabel.classList.remove('value-shown');
+
+        setTimeout(() => {
+          regionLabel.textContent = regionName;
+          regionLabel.classList.remove('showing-value');
+        }, 150);
+      }
+    }
+  });
+
+  const regionValue = {
+    基隆市: 1,
+    臺北市: 2,
+    新北市: 3,
+    桃園市: 4,
+    宜蘭縣: 5,
+    新竹市: 6,
+    新竹縣: 7,
+    苗栗縣: 8,
+    臺中市: 9,
+    彰化縣: 10,
+    南投縣: 11,
+    雲林縣: 12,
+    嘉義市: 13,
+    嘉義縣: 14,
+    臺南市: 15,
+    高雄市: 16,
+    屏東縣: 17,
+    臺東縣: 18,
+    花蓮縣: 19,
+    澎湖縣: 20,
+    金門縣: 21,
+    連江縣: 22,
+  };
+
   const newsSwiper = new Swiper('.newsSwiper', {
     loop: true,
 
